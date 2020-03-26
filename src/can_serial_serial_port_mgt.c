7fed0381bb1104f7ff05d02a8cb29d0ee79a9d20
Clovis Durand <cd.clovel19@gmail.com>
2020-03-26 15:51:59 +0100

    Updated the serial port management code
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/can_serial_serial_port_mgt.c
+++ src/can_serial_serial_port_mgt.c
@@ -7,6 +7,7 @@
 /* Includes -------------------------------------------- */
 #include "can_serial_private.h"
 #include "can_serial_error_codes.h"
+#include "can_serial_commands.h"
 
 /* C system */
 #include <fcntl.h>  /* open, fcntl */
@@ -49,7 +50,7 @@ canSerialErrorCode_t CANSerial_initSerialPort(const canSerialID_t pID) {
     }
 
     errno = 0;
-    if(0 > fcntl(gCANSerial[pID].fd, F_SETFL, 0)) {/* ??? */
+    if(0 > fcntl(gCANSerial[pID].fd, F_SETFL, FNDELAY)) {/* ??? */
         /* Could not set the file descriptor's status flags */
         if(0 != errno) {
             printf("[ERROR] <CANSerial_initSerialPort> Failed to set the file descriptor's status flags\n");
@@ -57,6 +58,12 @@ canSerialErrorCode_t CANSerial_initSerialPort(const canSerialID_t pID) {
                 printf("        errno = %d (%s)\n", errno, strerror(errno));
             }
         }
+    }
+
+    /* Send the "Close CAN Channel" command */
+    if(CAN_SERIAL_ERROR_NONE != CANSerial_sendOpenChannelCmd(pID)) {
+        printf("[ERROR] <CANSerial_initSerialPort> Sending OPEN cmd failed !\n");
+        return CAN_SERIAL_ERROR_NET;
     }
 
     return CAN_SERIAL_ERROR_NONE;
@@ -68,14 +75,20 @@ canSerialErrorCode_t CANSerial_closeSerialPort(const canSerialID_t pID) {
         return CAN_SERIAL_ERROR_ARG;
     }
 
+    /* Send the "Close CAN Channel" command */
+    if(CAN_SERIAL_ERROR_NONE != CANSerial_sendCloseChannelCmd(pID)) {
+        printf("[ERROR] <CANSerial_closeSerialPort> Sending CLOSE cmd failed !\n");
+        return CAN_SERIAL_ERROR_NET;
+    }
+
     /* Close the socket */
     errno = 0;
     if(0 > close(gCANSerial[pID].fd)) {
-        printf("[ERROR] <CANSerial_initcanSocket> close failed !\n");
+        printf("[ERROR] <CANSerial_closeSerialPort> close failed !\n");
         if(0 != errno) {
             printf("        errno = %d (%s)\n", errno, strerror(errno));
         }
-        return CAN_SERIAL_ERROR_NET;
+        return CAN_SERIAL_ERROR_SYS;
     }
 
     return CAN_SERIAL_ERROR_NONE;
