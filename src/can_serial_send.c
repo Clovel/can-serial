6ddab510723e511cd2b2c4fccddbe9ca6b70475d
Clovis Durand <cd.clovel19@gmail.com>
2020-03-26 16:41:54 +0100

    Moved send message command to can_serial_send.c
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/can_serial_send.c
+++ src/can_serial_send.c
@@ -7,6 +7,7 @@
 /* Includes -------------------------------------------- */
 #include "can_serial_private.h"
 #include "can_serial_error_codes.h"
+#include "can_serial_commands.h"
 
 /* C system */
 #include <stddef.h>
@@ -34,35 +35,110 @@ canSerialErrorCode_t CANSerial_send(const canSerialID_t pID,
     const uint8_t * const pData,
     const uint32_t pFlags)
 {
-    pthread_mutex_lock(&gCANSerial[pID].mutex);
-
-    /* Check the ID */
-    if(pID != gCANSerial[pID].instanceID) {
+    if(!CANSerial_moduleExists(pID)) {
         printf("[ERROR] <CANSerial_send> No CANSerial module has the ID %u\n", pID);
         return CAN_SERIAL_ERROR_ARG;
     }
 
-    /* Check if the module is already initialized */
-    if(!gCANSerial[pID].isInitialized) {
-        printf("[ERROR] <CANSerial_rxThread> CANSerial module %u is not initialized.\n", gCANSerial[pID].instanceID);
-        return CAN_SERIAL_ERROR_NOT_INIT;
+    if(NULL == pMsg) {
+        printf("[ERROR] <CANSerial_send> Message ptr is NULL\n");
+        return CAN_SERIAL_ERROR_ARG;
     }
 
-    /* Build CANSerial message */
-    canMessage_t lMsg;
-    memset(lMsg.data, 0, CAN_MESSAGE_MAX_SIZE);
-    lMsg.id    = pCANID;
-    lMsg.size  = pSize;
-    lMsg.flags = pFlags;
-    for(uint8_t i = 0U; (i < lMsg.size) && (i < CAN_MESSAGE_MAX_SIZE) && (NULL != pData); i++) {
-        lMsg.data[i] = pData[i];
+    if(CAN_MESSAGE_MAX_SIZE < pSize) {
+        printf("[ERROR] <CANSerial_send> CAN ID out of bounds (%u)\n", pSize);
+        return CAN_SERIAL_ERROR_ARG;
     }
 
-    ssize_t lSentBytes = 0;
+    char lCmd[CAN_SERIAL_ANSWER_MAX_SIZE];
+    char lBuf[CAN_SERIAL_ANSWER_MAX_SIZE];
 
-    errno = 0;
+    /* Build the command string.
+     * The format for a CAN message is : 
+     * 
+     * For a standard CAN frame : 
+     * tiiildd..[CR]
+     * with :
+     * - t : command identifier : standard frame
+     * - iii : CAN-ID in HEX (11 bits, 0x000-0x7FF)
+     * - l : DLC
+     * - dd.. : Byte value in hex (00-FF).
+     *
+     * For an extended CAN frame :
+     * Tiiiiiiiildd..[CR]
+     * - T : command identifier : extended frame
+     * - iiiiiiii : CAN-ID in HEX (29 bits, 00000000-1FFFFFFF)
+     * - l : DLC
+     * - dd.. : Byte value in hex (00-FF).
+     * 
+     * Numbers of dd pairs must match the data length, otherwise an error will occur.
+     * [CR] is the end of command identifier.
+     */
+    if(CAN_SERIAL_ID_EXT_FLAG == (CAN_SERIAL_ID_MASK & pFlags)) {
+        /* Extended ID */
+
+        if(CAN_SERIAL_ID_EXT_MASK < pCANID) {
+            printf("[ERROR] <CANSerial_send> CAN ID out of bounds (%lu)\n", pCANID);
+            return CAN_SERIAL_ERROR_ARG;
+        }
+
+        /* Command identifier */
+        *lCmd = 'T';
+        lCmd++;
+
+        /* CAN ID */
+        snprintf(lCmd, 8U, "%X", pID);
+        lCmd += 8U;
+    } else {
+        /* Standard ID */
+        if(CAN_SERIAL_ID_STD_MASK < pCANID) {
+            printf("[ERROR] <CANSerial_send> CAN ID out of bounds (%lu)\n", pCANID);
+            return CAN_SERIAL_ERROR_ARG;
+        }
+
+        /* Command identifier */
+        *lCmd = 't';
+        lCmd++;
+
+        /* CAN ID */
+        snprintf(lCmd, 3U, "%X", pID);
+        lCmd += 3U;
+    }
+
+    /* DLC */
+    *lCmd = pSize;
+    lCmd++;
+
+    /* Data */
+    for(uint8_t i = 0U; (i < pSize); i++) {
+        snprintf(lCmd, 1U, "%X", *(pData + i));
+        lCmd++;
+    }
+
+    /* End-of-command CR */
+    *lCmd = CAN_SERIAL_END_OF_CMD;
+
+    pthread_mutex_lock(&gCANSerial[pID].mutex);
+
+    /* Send the command */
+    if(CAN_SERIAL_ERROR_NONE != CANSerial_sendCmd(pID, CAN_SERIAL_CMD_CLOSE, lBuf)) {
+        printf("[ERROR] <CANSerial_send> Failed to send the command \"%s\"\n", CAN_SERIAL_CMD_OPEN);
+        return CAN_SERIAL_ERROR_SYS;
+    }
 
     pthread_mutex_unlock(&gCANSerial[pID].mutex);
 
-    return CAN_SERIAL_ERROR_NONE;
+    /* Check the answer */
+    if(0 == strncmp(CAN_SERIAL_CMD_OK, lBuf, sizeof(CAN_SERIAL_CMD_OK))) {
+        /* Got "OK " */
+        return CAN_SERIAL_ERROR_NONE;
+    } else if(0 == strncmp(CAN_SERIAL_CMD_BELL, lBuf, sizeof(CAN_SERIAL_CMD_BELL))) {
+        /* Got "Bell" */
+        printf("[ERROR] <CANSerial_send> Got \"BELL\" (error) from device");
+        return CAN_SERIAL_ERROR_NET;
+    } else {
+        /* Unknown answer... */
+        printf("[ERROR] <CANSerial_send> Got unknown answer from device : %s", lBuf);
+        return CAN_SERIAL_ERROR_NET;
+    }
 }
